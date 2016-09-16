5dfca90956c86719204d11996a99f01036f14d82
sysadmin <sysadmin@sw-entwicklung.informatik.hs-furtwangen.de>
2016-09-16 11:16:33 +0200

    initial comit

---
+++ test/src/main/java/tesst/test/App.java
@@ -0,0 +1,18 @@
+package tesst.test;
+import org.junit.Test;
+
+
+public class App 
+{
+	@Test
+	public void  ext () {
+		
+		System.out.println("TEST_1:  Successful");
+		
+	}
+		
+	public void main (String[] args) {
+		
+		ext();
+	}
+}
